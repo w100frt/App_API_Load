7f6091b3450af892c410493daeb74b33624d1f1b
w100frt <41997349+w100frt@users.noreply.github.com>
2019-10-01 11:42:40 -0400

    Update my-functions.js

--- my-functions.js
+++ my-functions.js
@@ -12,7 +12,7 @@ const fs = require('fs');
   function logBody(requestParams, response, context, ee, next) {
 console.log(response.body);
       let body=response.body+"\r\n"; //this enabled new line in windows , use \n for linux
-	fs.appendFile('temp.json', body, function(err, body){
+	fs.appendFile('temp.txt', body, function(err, body){
     if (err) console.log(err);
     //console.log("Successfully Written to File.");
 	});
