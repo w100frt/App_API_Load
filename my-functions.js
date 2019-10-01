12af0fa04d50ee8cf1b0a9d8002d9e4a46860465
w100frt <41997349+w100frt@users.noreply.github.com>
2019-10-01 11:23:39 -0400

    Create my-functions.js

---
+++ my-functions.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+ module.exports = {
+    setJSONBody: setJSONBody,
+    logBody: logBody
+  }
+
+  function setJSONBody(requestParams, context, ee, next) {
+    return next(); // MUST be called for the scenario to continue
+  }
+
+  function logBody(requestParams, response, context, ee, next) {
+console.log(response.body);
+      let body=response.body+"\r\n"; //this enabled new line in windows , use \n for linux
+	fs.appendFile('temp.txt', body, function(err, body){
+    if (err) console.log(err);
+    //console.log("Successfully Written to File.");
+	});
+    return next(); // MUST be called for the scenario to continue
+  }
