6f1afc526b9d78c03232bf21786cb54a5ce224a0
w100frt <41997349+w100frt@users.noreply.github.com>
2019-10-02 09:53:54 -0400

    Update my-functions.js

--- my-functions.js
+++ my-functions.js
@@ -9,10 +9,10 @@ const fs = require('fs');
     return next(); // MUST be called for the scenario to continue
   }
 
-  function logBody(requestParams, response,  next) {
+  function logBody(requestParams, response, context, ee, next) {
 console.log(response.body);
       let body=response.body+"\r\n"; //this enabled new line in windows , use \n for linux
-	fs.appendFile('testLogs.txt', body, function(err){
+	fs.appendFile('testLogs.txt', function(err){
     if (err) console.log(err);
     //console.log("Successfully Written to File.");
 	});
