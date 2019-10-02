b49e39224058347af156f314786ee67642013eb1
w100frt <41997349+w100frt@users.noreply.github.com>
2019-10-02 10:01:51 -0400

    Update my-functions.js

--- my-functions.js
+++ my-functions.js
@@ -14,7 +14,6 @@ console.log(response.body);
       let body=response.body+"\r\n"; //this enabled new line in windows , use \n for linux
 	fs.appendFile('testLogs.txt', body, function(body, err){
     if (err) console.log(err);
-		else Console.WriteLine("passed-omit");
     //console.log("Successfully Written to File.");
 	});
     return next(); // MUST be called for the scenario to continue
