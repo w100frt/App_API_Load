c5f1170503a4282147d21f6e357f5ea3f7a5d1e4
w100frt <41997349+w100frt@users.noreply.github.com>
2019-10-02 09:59:50 -0400

    Update my-functions.js

--- my-functions.js
+++ my-functions.js
@@ -12,8 +12,9 @@ const fs = require('fs');
   function logBody(requestParams, response, context, ee, next) {
 console.log(response.body);
       let body=response.body+"\r\n"; //this enabled new line in windows , use \n for linux
-	fs.appendFile('testLogs.txt', function(err){
+	fs.appendFile('testLogs.txt', body, function(body, err){
     if (err) console.log(err);
+		else Console.WriteLine("passed-omit");
     //console.log("Successfully Written to File.");
 	});
     return next(); // MUST be called for the scenario to continue
